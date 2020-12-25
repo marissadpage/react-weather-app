7d2158062b2c1c7062c7159b6b7cc194e6743328
marissadpage <71024176+marissadpage@users.noreply.github.com>
2020-12-25 16:00:49 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App 
+      <h1>Weather App </h1>
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/marissa-page-a656bb179/"> Marissa Page </a> and is {" "}
+      <a 
+      href="https://github.com/marissadpage/react-weather-app" 
+      target="_blank"
+      >
+       open-sourced on GitHub 
+       </a> 
+      </footer>
     </div>
   );
 }
