5772ed3121f771703bdde2bbd1b606944ea423f7
marissadpage <71024176+marissadpage@users.noreply.github.com>
2020-12-29 20:36:28 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App </h1>
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/marissa-page-a656bb179/"> Marissa Page </a> and is {" "}
@@ -14,6 +15,7 @@ export default function App() {
        open-sourced on GitHub 
        </a> 
       </footer>
+      </div>
     </div>
   );
 }
