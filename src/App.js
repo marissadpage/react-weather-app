99c19bd70dc492d65c34bfd324510b6b7f3cf15c
marissadpage <71024176+marissadpage@users.noreply.github.com>
2020-12-30 15:33:42 -0500

    Removed Target Blank Errors

--- src/App.js
+++ src/App.js
@@ -11,14 +11,12 @@ export default function App() {
         This project was coded by <a href="https://www.linkedin.com/in/marissa-page-a656bb179/"> Marissa Page </a> and is {" "}
       <a 
       href="https://github.com/marissadpage/react-weather-app" 
-      target="_blank"
       >
        open-sourced on GitHub 
        </a> {" "}
           and{" "}
           <a
             href="https://romantic-noyce-de948c.netlify.app"
-            target="_blank"
           >
             hosted on Netlify
           </a>
